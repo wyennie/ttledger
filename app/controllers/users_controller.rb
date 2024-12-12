f3a611f7a9b0e6561a0edd781d4d451555ac9700
Will Yennie <wryennie@gmail.com>
2024-12-12 01:24:58 -0500

    feature: displays users characters on their home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @campaigns = @user.campaigns
+    @characters = @user.characters.includes(:campaign)
   end
 
   def new
