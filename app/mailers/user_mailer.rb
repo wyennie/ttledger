47b7a109616672f95892278e6916a385b3f27fac
Will Yennie <wryennie@gmail.com>
2024-12-16 17:28:43 -0500

    chore: cleaned up code for linting

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'noreply@example.com'
+  default from: "noreply@example.com"
 
   def confirmation_email(user)
     @user = user
     @url = confirm_email_url(token: @user.confirmation_token)
-    mail(to: @user.email, subject: 'Email Confirmation')
+    mail(to: @user.email, subject: "Email Confirmation")
   end
 end
