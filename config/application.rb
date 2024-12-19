d9096f3df20d5e25e1a1cd120e9a825ecc8ebd24
Will Yennie <wryennie@gmail.com>
2024-12-18 22:39:51 -0500

    chore: quote preference for linting

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,6 @@ module CharCraft
 
     # Config for postmark amil server
     config.action_mailer.delivery_method = :postmark
-    config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_TOKEN'] }
+    config.action_mailer.postmark_settings = { api_token: ENV["POSTMARK_API_TOKEN"] }
   end
 end
