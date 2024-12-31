ab2d3b6e1a1c9808b2bb7850e4f25548e078f6a0
Will Yennie <wryennie@gmail.com>
2024-12-31 18:48:24 -0500

    migrate main database from sqlite to postgres

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -4,8 +4,8 @@ export default class extends Controller {
   static targets = ["chatMessage"]
 
   clearInput(event) {
-    console.log("hello from chat-controller")
-    console.log(this.chatMessageTarget)
-    this.chatMessageTarget.value = ""; // Clears the input field
+    setTimeout(() => {
+      this.chatMessageTarget.value = "";
+    }, 0);
   }
 }
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2024_12_30_011400) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "armors", force: :cascade do |t|
     t.integer "ac_bonus"
     t.integer "check_penalty"
