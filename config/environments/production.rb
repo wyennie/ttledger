2c8126f909db57e529ebc4fdfab9704b292126e6
Will Yennie <wryennie@gmail.com>
2025-01-17 14:15:43 -0500

    testing non-precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,8 +17,9 @@ Rails.application.configure do
 
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
-  config.assets.compile = false # Assets must be precompiled
+  config.assets.compile = true # Assets must be precompiled
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.assets.debug = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
