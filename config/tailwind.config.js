e7f89ce3a07e558e8975fabc924f5dc3eceede88
Will Yennie <wryennie@gmail.com>
2025-01-17 15:58:20 -0500

    typo

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './public/*.html.erb',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
     '.app/assets/sylesheets/**/*.css'
   ],
   theme: {
