a6adf945824de098343ea897c783617b67d3fd1c
Will Yennie <wryennie@gmail.com>
2025-01-21 16:34:48 -0500

    add transfer encoding response heading

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -34,6 +34,7 @@ def chat_response
   logger.info "Setting response headers..."
   response.headers["Content-Type"]  = "text/event-stream"
   response.headers["Content-Encoding"] = "chunked"
+  response.headers["Transfer-Encoding"] = "chunked"
   logger.info "Response headers set: #{response.headers}"
 
   logger.info "Initializing SSE and ChatService..."
